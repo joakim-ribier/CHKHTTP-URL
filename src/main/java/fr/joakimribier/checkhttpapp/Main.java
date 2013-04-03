0afe87b67963d168860b2c0b2aaddd585708e3cf
Joakim Ribier <joakim.ribier@gmail.com>
2013-04-03 20:02:55 +0600

    add Apache License Version 2.0, January 2004

--- src/main/java/fr/joakimribier/checkhttpapp/Main.java
+++ src/main/java/fr/joakimribier/checkhttpapp/Main.java
@@ -22,28 +22,24 @@ import fr.joakimribier.checkhttpapp.services.SendMailService;
 import fr.joakimribier.checkhttpapp.utils.FileResourceService;
 
 /**
- * CHKHTTP-URL is an application which check the accessibility of http url
  * 
- * The configuration file path default is defined in file.path
- * OR
- * It can be defined by argument ( args[0] )
+ * Copyright 2013 Joakim Ribier
  * 
  * 
- * Configuration file :
- * 
- * check_url=http://www.my-domain.com
- * smtp=host.smpt.com
- * username=from@my-domain.com
- * password=password
- * from=from@my-domain.com
- * to=to@my-domain.com
- * subject=It's the subject
- * text=Content plain text
- * smtp_debug=true
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  * 
  * 
- * @author Joakim Ribier
- *
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
  */
 public class Main {
 	
